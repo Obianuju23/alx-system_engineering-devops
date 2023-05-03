079875f4cab4654da345cc204d12802dfbef6168
Obianuju23 <obianujunmoh@gmail.com>
2023-05-03 12:34:47 -0700

    Find the regular expression that will match the above cases.Using the project instructions, create a Ruby script that accepts one argument and pass it to a regular.expression matching method

---
+++ 0x06-regular_expressions/3-repetition_token_2.rb
@@ -0,0 +1,6 @@
+#!/usr/bin/env ruby
+"""
+This is a Ruby Script using regexp to match cases as shown in the example that accepts one argument and pass it to a regular expression matching method
+"""
+
+puts ARGV[0].scan(/hbt{1,4}n/).join

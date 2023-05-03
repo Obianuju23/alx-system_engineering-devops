4cbba060803c42fa87b63468d589fa4e9ea7cb6f
Obianuju23 <obianujunmoh@gmail.com>
2023-05-03 13:48:00 -0700

    The regular expression must be exactly matching a string that starts with h ends with n and can have any single character in between.Using the project instructions, create a Ruby script that accepts one argument and pass it to a regular expression matching method

---
+++ 0x06-regular_expressions/5-beginning_and_end.rb
@@ -0,0 +1,6 @@
+#!/usr/bin/env ruby
+"""
+This is a Ruby script that accepts one argument and pass it to a regular expression matching method which uses regexp to match a string that starts with h ends with n and can have any single character in between Using the project instructions.
+"""
+
+puts ARGV[0].scan(/h[a-z0-9A-Z]/n/).join

4b930a48f9533f3b78f1d2e98f87535c9f47b1c4
Obianuju23 <obianujunmoh@gmail.com>
2023-08-08 12:36:34 -0700

    Function to query subscribers on a given Reddit subreddit

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,11 +8,10 @@ import requests
 
 def number_of_subscribers(subreddit):
     """Returns total number of subscribers in a given subreddit"""
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {'User-Agent': 'Custom User Agent'}
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {'User-Agent': 'My Customised User Agent'}
         response = requests.get(url, headers=headers, allow_redirects=False)
-        if response.status_code == 200:
-            result = response.json().get("data")
-        else
+        if response.status_code == 404:
             return 0
+        result = response.json().get("data")
         return result.get("subscribers")

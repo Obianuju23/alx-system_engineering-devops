d18352a7773bff7eb20fcf09d2e73bf149d762ce
Obianuju23 <obianuju23@gmail.com>
2023-08-18 05:03:07 +0000

    changed soft and hard nofile

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,7 +1,7 @@
-ncrease the limit for holberton user
+#increase the limit for holberton user
 
 exec { 'increase hard limit':
-    command => "sed -i 's/holberton hard nofile 5/holberton hard nofile 4096/g' /etc/security/limits.conf",
+    command => "sed -i 's/holberton hard nofile 5/holberton hard nofile 5000/g' /etc/security/limits.conf",
     path    => '/usr/bin:/usr/sbin:/bin:/sbin',
 }
 exec { 'increase soft limit':
